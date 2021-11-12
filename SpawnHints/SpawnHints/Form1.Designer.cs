8976214a91ed63987503a32108d2662b305cca10
DeltaDizzy <EJBraswell@gmail.com>
2021-11-12 07:36:04 -0600

    spawnhints

---
+++ SpawnHints/SpawnHints/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace SpawnHints
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrMove = new System.Windows.Forms.Timer(this.components);
+            this.tmrBox = new System.Windows.Forms.Timer(this.components);
+            this.lblScore = new System.Windows.Forms.Label();
+            this.jeff = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.jeff)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tmrMove
+            // 
+            this.tmrMove.Enabled = true;
+            this.tmrMove.Interval = 20;
+            this.tmrMove.Tick += new System.EventHandler(this.tmrMove_Tick);
+            // 
+            // tmrBox
+            // 
+            this.tmrBox.Enabled = true;
+            this.tmrBox.Interval = 1000;
+            this.tmrBox.Tick += new System.EventHandler(this.tmrBox_Tick);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(12, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(64, 25);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "label1";
+            // 
+            // jeff
+            // 
+            this.jeff.BackColor = System.Drawing.SystemColors.Control;
+            this.jeff.Location = new System.Drawing.Point(32, 142);
+            this.jeff.Name = "jeff";
+            this.jeff.Size = new System.Drawing.Size(59, 58);
+            this.jeff.TabIndex = 0;
+            this.jeff.TabStop = false;
+            this.jeff.Move += new System.EventHandler(this.jeff_Move);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.jeff);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.jeff)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox jeff;
+        private System.Windows.Forms.Timer tmrMove;
+        private System.Windows.Forms.Timer tmrBox;
+        private System.Windows.Forms.Label lblScore;
+    }
+}
+
---
+++ SpawnHints/SpawnHints/Form1.cs
@@ -0,0 +1,298 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpawnHints
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// up, down, left, right
+        /// </summary>
+        (bool, bool, bool, bool) moveState = (false, false, false, false);
+        int speed = 5;
+        List<PictureBox> spuds = new List<PictureBox>();
+        List<PictureBox> boxes = new List<PictureBox>();
+        Random r = new Random();
+        MySqlConnection connection = new MySqlConnection("Server=50.62.209.85;Initial Catalog='cs3data';Username=dbaseuser;Password=Castl3H1!!;");
+        MySqlCommand cmd = new MySqlCommand();
+        int total = 0;
+        Dictionary<PictureBox, float> spudDistances = new Dictionary<PictureBox, float>();
+        Dictionary<PictureBox, float> boxDistances = new Dictionary<PictureBox, float>();
+        int ticks = 0;
+        private int iter;
+        string dirName = "";
+
+        private void SpawnProj()
+        {
+            PictureBox box = new PictureBox();
+            box.Width = 50/5;
+            box.Height = 50/5;
+            box.BackColor = Color.Brown;
+            //box.Location = new Point(r.Next(0, ClientSize.Width - box.Width), r.Next(0, ClientSize.Height - box.Height));
+            Point leftSpud = new Point(jeff.Location.X, jeff.Location.Y + jeff.Height / 2);
+            Point rightSpud = new Point(jeff.Location.X + jeff.Width, jeff.Location.Y + jeff.Height / 2);
+            Point upSpud = new Point(jeff.Location.X + jeff.Width / 2, jeff.Location.Y);
+            Point downSpud = new Point(jeff.Location.X + jeff.Width / 2, jeff.Location.Y + jeff.Height);
+            switch (dirName)
+            {
+                case "up":
+                    box.Location = upSpud;
+                    break;
+                case "down":
+                    box.Location = downSpud;
+                    break;
+                case "left":
+                    box.Location = leftSpud;
+                    break;
+                case "right":
+                    box.Location = rightSpud;
+                    break;
+                default:
+                    break;
+            }
+            spuds.Add(box);
+            Controls.Add(box);
+        }
+
+        private void SpawnBox()
+        {
+            PictureBox box = new PictureBox();
+            box.Width = 50;
+            box.Height = 50;
+            box.BackColor = Color.Brown;
+            box.Location = new Point(r.Next(box.Width+15, ClientSize.Width - box.Width), r.Next(0, ClientSize.Height - box.Height));
+            boxes.Add(box);
+            Controls.Add(box);
+        }
+
+
+        private void ThrowSpud()
+        {
+            switch (dirName)
+            {
+                case "up":
+                    for (int i = 0; i < spuds.Count; i++)
+                    {
+                        spuds[i].Top -= speed;
+                    }
+                    break;
+                case "down":
+                    for (int i = 0; i < spuds.Count; i++)
+                    {
+                        spuds[i].Top += speed;
+                    }
+                    break;
+                case "left":
+                    for (int i = 0; i < spuds.Count; i++)
+                    {
+                        spuds[i].Left -= speed;
+                    }
+                    break;
+                case "right":
+                    for (int i = 0; i < spuds.Count; i++)
+                    {
+                        spuds[i].Left += speed;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            
+            CollisionCheck();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W: // up
+                    moveState.Item1 = true; ;
+                    break;
+                case Keys.A: // left
+                    moveState.Item3 = true;
+                    break;
+                case Keys.S: // down
+                    moveState.Item2 = true;
+                    break;
+                case Keys.D: //right
+                    moveState.Item4 = true;
+                    break;
+                case Keys.Space:
+                    SpawnProj();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W: // up
+                    moveState.Item1 = false;
+                    break;
+                case Keys.A: // left
+                    moveState.Item3 = false;
+                    break;
+                case Keys.S: // down
+                    moveState.Item2 = false;
+                    break;
+                case Keys.D: //right
+                    moveState.Item4 = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void tmrMove_Tick(object sender, EventArgs e)
+        {
+            if (moveState.Item1 == true)
+            {
+                jeff.Top -= speed;
+            }
+            if (moveState.Item2 == true)
+            {
+                jeff.Top += speed;
+            }
+            if (moveState.Item3 == true)
+            {
+                jeff.Left -= speed;
+            }
+            if (moveState.Item4 == true)
+            {
+                jeff.Left += speed;
+            }
+            lblScore.Text = total.ToString();
+            ThrowSpud();
+            // get direction
+            int dir = Convert.ToInt32(moveState.Item1) * 8 + Convert.ToInt32(moveState.Item2) * 4
+                + Convert.ToInt32(moveState.Item3) * 2 + Convert.ToInt32(moveState.Item4) * 1;
+            switch (dir)
+            {
+                case 8:
+                    dirName = "up";
+                    break;
+                case 4:
+                    dirName = "down";
+                    break;
+                case 2:
+                    dirName = "left";
+                    break;
+                case 1:
+                    dirName = "right";
+                    break;
+                default:
+                    break;
+            }
+            if (ticks % 20 == 0)
+            {          
+                if (iter >= 4) iter = 1;
+                if (dirName == "")
+                {
+                    //dirName = "down";
+                    return;
+                }
+                jeff.Image = (Image)Properties.Resources.ResourceManager.GetObject(dirName + iter);
+                iter++;
+            }
+            ticks++;
+            
+        }
+
+        private void CollisionCheck()
+        {
+            PictureBox target;
+            if (spuds.Count > 0 && boxes.Count > 0)
+            {
+                for (int i = 0; i < boxes.Count; i++)
+                {
+                    for (int j = 0; j < spuds.Count; j++)
+                    {
+                        if (i < boxes.Count)
+                        {
+                            if (boxes[i].Bounds.IntersectsWith(spuds[j].Bounds))
+                            {
+                                target = boxes[i];
+                                Controls.Remove(target);
+                                boxes.RemoveAt(i);
+                                target.Dispose();
+                                target = spuds[j];
+                                Controls.Remove(target);
+                                spuds.RemoveAt(j);
+                                target.Dispose();
+                                total++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void tmrBox_Tick(object sender, EventArgs e)
+        {
+            SpawnBox();
+        }
+
+        private void jeff_Move(object sender, EventArgs e)
+        {
+            if (spuds.Count == 0)
+            {
+                return;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.SetStyle(ControlStyles.DoubleBuffer, true);
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.UpdateStyles();
+        }
+
+        private void RefreshTable()
+        {
+            string query = "SELECT * FROM elijah";
+            // import
+            connection.Open();
+            //MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
+            //dt.Clear();
+            //dgvData.DataSource = null;
+            //adapter.Fill(dt);
+            //dgvData.DataSource = dt;
+            connection.Close();
+        }
+
+        private void UpdateTable()
+        {
+            string query = $"UPDATE elijah SET quantity = {total} WHERE Id = 1";
+            // $"UPDATE elijah SET quantity = {total} WHERE Id = 1";
+            connection.Open();
+            cmd = new MySqlCommand(query, connection);
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UpdateTable();
+        }
+    }
+}
